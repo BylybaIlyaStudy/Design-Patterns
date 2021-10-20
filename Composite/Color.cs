5a2a0c45b6625c8fc1ef7aa23609284981dad974
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-20 21:21:58 +0700

    add composite realization

---
+++ Composite/Color.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Color : IRainbow
+    {
+        public HashSet<System.Drawing.Color> GetColors()
+        {
+            var random = new Random();
+
+            return new HashSet<System.Drawing.Color>()
+            {
+                System.Drawing.Color.FromArgb(
+                    random.Next(255),
+                    random.Next(255),
+                    random.Next(255))
+            };
+        }
+    }
+}
