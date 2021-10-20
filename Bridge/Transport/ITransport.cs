b8265a0048e51635f41a230981717455b1866153
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-20 20:15:41 +0700

    add bridge realization

---
+++ Bridge/Transport/ITransport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bridge.Transport.Engine;
+
+namespace Bridge.Transport
+{
+    public interface ITransport
+    {
+        public IEngine Engine { get; set; }
+        public int Wheels { get; set; }
+        public int Seats { get; set; }
+    }
+}
