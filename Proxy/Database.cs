54586c52266c60a2bc14f868990361bcc74bcb99
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-21 16:37:43 +0700

    add proxy realization

---
+++ Proxy/Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class Database : IDatabase
+    {
+        static private int countOfAccess = 0;
+        private string[] names;
+
+        static public int CountOfAccess => countOfAccess;
+
+        public Database(string[] names)
+        {
+            this.names = names;
+        }
+
+        public string GetName(int index)
+        {
+            countOfAccess++;
+
+            if (index >= 0 && index < names.Length)
+            {
+                return names[index];
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Proxy/ProxyDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class ProxyDatabase : IDatabase
+    {
+        private Dictionary<int, string> CachedNames = new();
+        private IDatabase database;
+
+        public ProxyDatabase(IDatabase database)
+        {
+            this.database = database;
+        }
+
+        public string GetName(int index)
+        {
+            if (CachedNames.ContainsKey(index))
+            {
+                return CachedNames[index];
+            }
+
+            var name = database.GetName(index);
+            CachedNames.Add(index, name);
+
+            return name;
+        }
+    }
+}
