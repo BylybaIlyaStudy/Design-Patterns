626cd129d8289535871c6b0b97021d73ec05bf61
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-21 13:55:20 +0700

    add facade reralization

---
+++ Facade/Car/ICar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.Car
+{
+    public interface ICar
+    {
+        public int Wheels { get; set; }
+        public int Engines { get; set; }
+        public int Seats { get; set; }
+        public TypeOfEngine TypeOfEngine { get; set; }
+    }
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Facade.Car;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar car = new Car.Car();
+            Converter converter = new();
+
+            car = new Random().Next() % 2 == 1
+                ? converter.ToCargoCar(car)
+                : converter.ToSportCar(car);
+
+            Console.WriteLine(car.TypeOfEngine);
+        }
+    }
+}
