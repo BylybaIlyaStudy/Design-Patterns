c1498bf9db495332d40440d6c662fdd5fc445cbc
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-21 13:31:36 +0700

    add decorator realization

---
+++ Decorator/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class Car : ICar
+    {
+        public string GetCarClass()
+        {
+            return "ordinary car";
+        }
+    }
+}
---
+++ Decorator/CargoCarDecorator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class CargoCarDecorator : ICar
+    {
+        private ICar car;
+
+        public CargoCarDecorator(ICar car)
+        {
+            this.car = car;
+        }
+
+        public string GetCarClass()
+        {
+            return car.GetCarClass() + "\n\t cargoMode:on";
+        }
+    }
+}
