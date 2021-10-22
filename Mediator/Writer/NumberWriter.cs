711d5bc7fd03b601d1ee8d386949944c58829d0a
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-22 14:52:26 +0700

    add mediator realization

---
+++ Mediator/Writer/NumberWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator.Writer
+{
+    public class NumberWriter : IWriter
+    {
+        public void PrintNumber()
+        {
+            Console.WriteLine(1);
+        }
+
+        public Mediator Mediator { get; set; }
+    }
+}
---
+++ Mediator/Writer/StringWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator.Writer
+{
+    public class StringWriter : IWriter
+    {
+        public void PrintString()
+        {
+            Console.WriteLine("ordinary string");
+        }
+
+        public Mediator Mediator { get; set; }
+    }
+}
