873253fb31d6738d0fdde6c832bd7a6553b2899f
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-22 15:14:06 +0700

    add memento realization

---
+++ Memento/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Smith")
+            {
+                Name = "Jhon"
+            };
+
+            Console.WriteLine(person.Name);
+
+            Snapshot snapshot = person.CreateSnapshot();
+
+            person.Name = "Tim";
+
+            Console.WriteLine(person.Name);
+
+            snapshot.Restore(person);
+
+            Console.WriteLine(person.Name);
+        }
+    }
+}
---
+++ Memento/Snapshot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    public class Snapshot
+    {
+        private string name;
+        private string surname;
+        public Snapshot(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public void Restore(Person person)
+        {
+            person.Restore(new Person(surname)
+            {
+                Name = name
+            });
+        }
+    }
+}
