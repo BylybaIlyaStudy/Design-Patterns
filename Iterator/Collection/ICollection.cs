57c0967685a091bc8899357a1230deee9e36ab3d
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-22 14:19:10 +0700

    add iterator realization

---
+++ Iterator/Collection/ICollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Iterator.Iterator;
+
+namespace Iterator.Collection
+{
+    public interface ICollection<T>
+    {
+        public IIterator<T> CreateForwardIterator();
+        public IIterator<T> CreateBackwardIterator();
+    }
+}
---
+++ Iterator/Iterator/IIterator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Iterator.Collection;
+
+namespace Iterator.Iterator
+{
+    public interface IIterator<T>
+    {
+        public T GetNext();
+
+        public bool HasMore();
+    }
+}
