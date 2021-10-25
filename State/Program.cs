3d6d95ab5723956a99156825a334c923dab5f611
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-25 18:50:48 +0700

    add state realization

---
+++ State/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using State.State;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new(new StateOne());
+            printer.Print();
+            printer.Print();
+            printer.State = new StateTwo();
+            printer.Print();
+        }
+    }
+}
---
+++ State/State/StateTwo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State.State
+{
+    public class StateTwo : IState
+    {
+        public void Print(Printer printer)
+        {
+            Console.WriteLine("State Two");
+            printer.State = new StateTwo();
+        }
+    }
+}
