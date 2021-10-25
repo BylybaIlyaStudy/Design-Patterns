3dec9767c214b7bc7b170f4ddd2b67a99310f9ed
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-25 18:14:53 +0700

    add observer realization

---
+++ Observer/EventManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class EventManager
+    {
+        private HashSet<ISubscribe> subscribers = new();
+
+        public void Subscribe(ISubscribe subscriber)
+        {
+            subscribers.Add(subscriber);
+        }
+
+        public void Unsubscribe(ISubscribe subscriber)
+        {
+            subscribers.Remove(subscriber);
+        }
+
+        public void Notify(EventType event_, string name)
+        {
+            foreach (var subscriber in subscribers)
+            {
+                subscriber.Update($"{name} was {event_}");
+            }
+        }
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventManager eventManager = new EventManager();
+            Person person1 = new Person("Tom", eventManager);
+            Person person2 = new Person("Tim", eventManager);
+            person1.Dispose();
+            person2.Dispose();
+        }
+    }
+}
