b2f5d74f73856e94da0423342d1e5178db0047df
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-25 19:59:17 +0700

    add template method realization

---
+++ Template Method/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Template_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new PrinterOne().Print();
+            new PrinterTwo().Print();
+        }
+    }
+}
