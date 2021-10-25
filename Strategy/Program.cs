5ba54290d16e37f8d962e8bb3c259eae91ecb45d
Bylyba Ilia <Ilia.Bylyba@infotecs.ru>
2021-10-25 19:43:28 +0700

    add strategy realization

---
+++ Strategy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Strategy.Strategy;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer printer = new Printer(new StrategyOne());
+            printer.Print();
+            printer.Print();
+            printer.Strategy = new StrategyTwo();
+            printer.Print();
+        }
+    }
+}
